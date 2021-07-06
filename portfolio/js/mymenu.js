ae0b26d5724da1b815c92418aeed461c964809f6
samriz <samriz@users.noreply.github.com>
2021-07-05 21:46:14 -0500

    Put menu.js and mymenu.js in strict mode.

--- portfolio/js/mymenu.js
+++ portfolio/js/mymenu.js
@@ -1,8 +1,11 @@
+"use strict";
 /* import PopulateFileWithMenuAsync from "./modules/menu.js"
 PopulateFileWithMenuAsync(".html"); */
 
 import Menu from "./modules/menu.js"
 
 var links = new Array("index", "about", "contact");
-const m = new Menu(links, ".html");
-m.PopulateFileWithMenu();
+/* const m = new Menu(links, ".html");
+m.PopulateFileWithMenu(); */
+
+new Menu(links, ".html").PopulateFileWithMenu();
