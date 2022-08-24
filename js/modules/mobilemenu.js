0a0a4aa489c8926d0b761e65619a2b682d46bc2a
samriz <samriz@users.noreply.github.com>
2022-08-23 23:08:43 -0500

    Update mobilemenu.js
    Got mobile menu to work.

--- js/modules/mobilemenu.js
+++ js/modules/mobilemenu.js
@@ -24,6 +24,9 @@ export default class MobileMenu
     console.log(anchors);
     const mobilemenu = document.querySelectorAll(".mobilemenu")[0];
     const select = document.createElement("select");
+    select.setAttribute("onchange", "location = this.value;");
+    //select.setAttribute("placeholder", "Menu");
+    //select.style.display = "none";
     for(let i = 0; i < anchors.length; i++)
     {
       if(i === 0)
@@ -32,7 +35,9 @@ export default class MobileMenu
         zeroOption.style.display = "none";
         select.appendChild(zeroOption);
       }
-      let newOption = new Option(anchors[i].innerText, anchors[i].href + fileExtension);
+      let newOption;
+      if(anchors[i].innerText === "inde" || anchors[i].innerText === "index") newOption = new Option("home", anchors[i].href + fileExtension);
+      else newOption = new Option(anchors[i].innerText + "t", anchors[i].href + fileExtension);
       select.appendChild(newOption);
     }
     mobilemenu.appendChild(select);
