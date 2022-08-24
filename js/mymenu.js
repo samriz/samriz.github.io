76f2d4a868c9f6d85efdbc84130c762dfca28361
samriz <samriz@users.noreply.github.com>
2022-08-24 10:04:32 -0500

    Changed mobile viewport to 640 and made form more responsive when viewport is resized.

--- js/mymenu.js
+++ js/mymenu.js
@@ -5,16 +5,22 @@ import MobileMenu from "./modules/mobilemenu.js";
 var links = new Array("index", "about", "contact");
 let nav = document.getElementsByTagName("nav")[0];
 
-if(window.visualViewport.width < 500)
-{    
-    if(nav.classList.contains("menu")) nav.classList.replace("menu", "mobilemenu");
-    const mobilemenu = new MobileMenu();
-    mobilemenu.createMobileMenuAsync(links, ".html");
-}
-else
+async function viewportResize()
 {
-    if(nav.classList.contains("mobilemenu")) nav.classList.replace("mobilemenu", "menu");
-    const menu = new Menu(links, ".html");
-    await menu.PopulateDocumentWithMenuAsync();
-    //console.log(m.GetAnchors());
+    if(window.visualViewport.width < 640)
+    {    
+        if(nav.classList.contains("menu")) nav.classList.replace("menu", "mobilemenu");
+        const mobilemenu = new MobileMenu();
+        mobilemenu.createMobileMenuAsync(links, ".html");
+    }
+    else
+    {
+        if(nav.classList.contains("mobilemenu")) nav.classList.replace("mobilemenu", "menu");
+        const menu = new Menu(links, ".html");
+        await menu.PopulateDocumentWithMenuAsync();
+        //console.log(m.GetAnchors());
+    }
 }
+
+await viewportResize();
+//window.onresize = viewportResize;
